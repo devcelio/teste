e16160c25027666940a7d19139a27b5dd6e8f6f4
Célio Júnior <celiojr.dev@gmail.com>
2025-03-10 16:18:38 -0300

    adição de listagem de produto

--- js/database.js
+++ js/database.js
@@ -1,50 +1,48 @@
 const PRODUTOS = [
     {
+      nome: "Álcool em gel",
+      fragrancia: "Lavanda",
+      descricao:
+        "Álcool em gel perfumado com fragrância suave de lavanda. Proporciona higienização eficaz enquanto deixa as mãos com um aroma agradável.",
+      composição: [
+        "Ethanol (Álcool 70%), Glycerin (Glicerina)",
+        "Prunus Amygdalus Oil (Óleo de Amêndoas)",
+        "Tocopheryl Acetate (Vitamina E)",
+        "Lavandula Angustifolia Oil (Óleo Essencial de Lavanda)",
+        "Water (Água)"
+      ],
+      indicacoes: "Higieniza as mãos de forma eficaz, eliminando impurezas enquanto hidrata e perfuma suavemente.",
+      modoUso: "Aplique uma pequena quantidade nas mãos e esfregue até a completa absorção. Dispensa o uso de água e toalha.",
+      advertencias: [
+        "Produto inflamável. Manter longe do fogo e de fontes de calor.",
+        "Uso externo. Evitar contato com os olhos.",
+        "Em caso de irritação, suspender o uso e procurar orientação médica."
+      ],
+      observacoes: "Manter fora do alcance de crianças e animais",
+      imagem: "assets/product-0-main.jfif",
+    },
+    {
         nome: "Álcool em gel",
         fragrancia: "Lavanda",
-        descricao: "Álcool em gel perfumado com fragrância suave de lavanda. Proporciona higienização eficaz enquanto deixa as mãos com um aroma agradável.",
-        preco: 13.99,
-        disponivel: false,
-        vendaProibida: true,
-        imagem: "https://exemplo.com/imagens/alcool-gel-lavanda.jpg"
-    },
-    {
-        nome: "Sabonete Líquido",
-        fragrancia: "Camomila",
-        descricao: "Sabonete líquido com extrato natural de camomila, proporcionando limpeza suave e hidratação para a pele.",
-        preco: 9.50,
-        disponivel: true,
-        vendaProibida: true,
-        imagem: "https://exemplo.com/imagens/sabonete-liquido-camomila.jpg"
-    },
-    {
-        nome: "Creme Hidratante",
-        fragrancia: "Amêndoas",
-        descricao: "Hidratante corporal enriquecido com óleo de amêndoas, deixando a pele macia e perfumada por horas.",
-        preco: 18.90,
-        disponivel: true,
-        vendaProibida: true,
-        imagem: "https://exemplo.com/imagens/creme-hidratante-amendoas.jpg"
-    },
-    {
-        nome: "Shampoo",
-        fragrancia: "Chá Verde",
-        descricao: "Shampoo refrescante com extrato de chá verde, promovendo limpeza profunda e fortalecimento dos fios.",
-        preco: 22.75,
-        disponivel: true,
-        vendaProibida: true,
-        imagem: "https://exemplo.com/imagens/shampoo-cha-verde.jpg"
-    },
+        descricao:
+          "Álcool em gel perfumado com fragrância suave de lavanda. Proporciona higienização eficaz enquanto deixa as mãos com um aroma agradável.",
+        composição: [
+          "Ethanol (Álcool 70%), Glycerin (Glicerina)",
+          "Prunus Amygdalus Oil (Óleo de Amêndoas)",
+          "Tocopheryl Acetate (Vitamina E)",
+          "Lavandula Angustifolia Oil (Óleo Essencial de Lavanda)",
+          "Water (Água)"
+        ],
+        indicacoes: "Higieniza as mãos de forma eficaz, eliminando impurezas enquanto hidrata e perfuma suavemente.",
+        modoUso: "Aplique uma pequena quantidade nas mãos e esfregue até a completa absorção. Dispensa o uso de água e toalha.",
+        advertencias: [
+          "Produto inflamável. Manter longe do fogo e de fontes de calor.",
+          "Uso externo. Evitar contato com os olhos.",
+          "Em caso de irritação, suspender o uso e procurar orientação médica."
+        ],
+        observacoes: "Manter fora do alcance de crianças e animais",
+        imagem: "assets/product-0-main.jfif",
+      },
 
-
-    // Novo produto
-    {
-        nome: "Novo produto",
-        fragrancia: "Exemplo de fragância",
-        descricao: "Exemplo de descrição",
-        preco: 0.00,
-        disponivel: false,
-        vendaProibida: false,
-        imagem: "https://exemplo.com/imagens/shampoo-cha-verde.jpg"
-    }
 ];
+  
--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,13 @@
 const router = new Navigo("/", { hash: true });
 
+
 router
   .on({
     "/": () => {
       loadPage(router, "/pages/home.html", { name: "Hello" });
     },
     "/produtos": () => {
-      loadPage(router, "/pages/products.html");
+      loadPage(router, "/pages/products.html", {PRODUTOS: PRODUTOS});
     },
   })
   .notFound(() => loadPage(router, "/pages/404.html"))

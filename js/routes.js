35cd2c1d151e96f6ee9238e9fc09c015f1b77c79
Célio Júnior <celiojr.dev@gmail.com>
2025-03-10 17:09:54 -0300

    adição de mais páginas

--- js/routes.js
+++ js/routes.js
@@ -1,13 +1,21 @@
 const router = new Navigo("/", { hash: true });
 
-
 router
   .on({
     "/": () => {
       loadPage(router, "/pages/home.html", { name: "Hello" });
     },
     "/produtos": () => {
-      loadPage(router, "/pages/products.html", {PRODUTOS: PRODUTOS});
+      loadPage(router, "/pages/products.html", { PRODUTOS: PRODUTOS });
+    },
+    "/produtos/:id": (params) => {
+      const product = PRODUTOS.find(p => p.id === params.id);
+      
+      if (product) {
+        loadPage(router, "/pages/product-details.html", { produto: product });
+      } else {
+        loadPage(router, "/pages/404.html");
+      }
     },
   })
   .notFound(() => loadPage(router, "/pages/404.html"))
